e24b4649fac9a75c6a29c87aa0dfd2c1f7bba460
Gabriel Boldi <110861595+GABRIELBOLDIVEIGA@users.noreply.github.com>
2023-02-27 18:00:51 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,54 +5,59 @@ import uuid from "react-uuid";
 import { useState, useEffect } from "react";
 import Sala from "./components/Sala";
 import LoadingSpinner from "./components/LoadingSpinner/index";
+import Clock from "react-live-clock";
+
+import dados_mok from "./json/DADOS_mok.json";
 
 export default function App() {
-    const segundos = 1000 * 60; // 1000 = 1 segundo
-    const [salas, setSalas] = useState([]);
-    const [finishedTimeout, setFinishedTimeout] = useState(false);
-    const [estilo, setEstilo] = useState();
+  const segundos = 1000 * 10; // 1000 = 1 segundo
+  const [salas, setSalas] = useState([]);
+  const [finishedTimeout, setFinishedTimeout] = useState(false);
+  const [estilo, setEstilo] = useState();
 
-    function carregarEventos() {
-        var dom_eventos = document.getElementById("conteudo").value;
-        
-        ordenaPorSala(dom_eventos);
-        setEstilo({display: "none"})
-    }
+  function carregarEventos() {
+    var dom_eventos = document.getElementById("conteudo").value;
 
-    function ordenaPorSala(dom_eventos) {
-        dom_eventos.sort(function (x, y) {
-            return x[0].numeroSala - y[0].numeroSala;
-        });
+    ordenaPorSala(dom_eventos);
+    setEstilo({ display: "none" });
+  }
 
-        setSalas(dom_eventos);
-    }
+  function ordenaPorSala(dom_eventos) {
+    dom_eventos.sort(function (x, y) {
+      return x[0].numeroSala - y[0].numeroSala;
+    });
 
-    useEffect(() => {
-        const id = setTimeout(() => {
-            setFinishedTimeout(true);
-            carregarEventos();
-        }, 20000);
+    setSalas(dom_eventos);
+  }
 
-        return () => clearTimeout(id);
-    }, []);
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setFinishedTimeout(true);
+      carregarEventos();
+    }, 20000);
 
-    
-    return (
-        <div className="app">
-            <Carousel>
-                {salas.map((sala) => {
-                    return (
-                        <Carousel.Item interval={segundos} key={uuid()}>
-                            <Sala key={uuid()} sala={sala} />
-                        </Carousel.Item>
-                    );
-                })}
-            </Carousel>
+    return () => clearTimeout(id);
+  }, []);
 
-            <div className="div_loadApp" style={estilo}>
-                {!finishedTimeout && <LoadingSpinner />}
-                <button className="bnt_loadApp" onClick={carregarEventos} />
-            </div>
-        </div>
-    );
+  console.log(dados_mok);
+  return (
+    <div className="app">
+         <Clock className="clock" format={"HH:mm:ss"} ticking={true} />
+      <Carousel>
+       
+        {salas.map((sala) => {
+          return (
+            <Carousel.Item interval={segundos} key={uuid()}>
+              <Sala key={uuid()} sala={sala} />
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+
+      <div className="div_loadApp" style={estilo}>
+        {!finishedTimeout && <LoadingSpinner />}
+        <button className="bnt_loadApp" onClick={carregarEventos} />
+      </div>
+    </div>
+  );
 }
