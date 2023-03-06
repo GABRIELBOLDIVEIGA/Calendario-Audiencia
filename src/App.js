b3a47bbea2b5a75b6c9613e801eacbc7791e94d0
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-06 19:45:19 -0300

    devido a um bloqueio da rede do forum o loop de atualizacao esta desativado

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-import { addDays, addNome, numeroSala, fitraHorario, filtraData } from "common/functions/functions";
-
 import "./App.module.scss";
 import ApiCalendar from "react-google-calendar-api";
 import { useEffect, useState, useRef } from "react";
 import { configApiCalendar } from "./common/config/config.js";
+import { addDays, addNome, numeroSala, fitraHorario, filtraData } from "common/functions/functions";
 import Sala from "./components/Sala/index";
 import uuid from "react-uuid";
 import Clock from "react-live-clock";
-
-// import "bootstrap/dist/css/bootstrap.css";
-
 import style from "./App.module.scss";
-import LoadingSpinner from "components/LoadingSpinner";
-import styled from "styled-components";
 
 export default function App() {
     const [usuarios, setUsuarios] = useState([]);
-    const [salasID, setSalasID] = useState([
-        // { id: "861e66117ea06082b7fbe7fcd32ad16f28fc46983bdb62151242ec5c3b12dc74@group.calendar.google.com", sala: "Sala 1" },
-        // { id: "810e981baa2a07de32b772cf4cea3ecc706bd27bd261a5d8edc91340654d0d53@group.calendar.google.com", sala: "Sala 2" },
-        // { id: "0fbf0cd46cc45ddb70f36d1c82b7e4807af21613471f5ec466d285449d872064@group.calendar.google.com", sala: "Sala 3" },
-    ]);
+    const [salasID, setSalasID] = useState([]);
     const [salas, setSalas] = useState([]);
     const [resultadoConsulta, setResultadoConsulta] = useState([]);
     const resultadoRef = useRef(resultadoConsulta);
@@ -125,19 +114,15 @@ export default function App() {
         buscarEventosDeCadaCalendario();
         setTimeout(RenderizaInfoFiltradaDosEventos, 1000 * 10); // Esse timeout serve para esperar 30 segundos ate a resposta   da API, provavel que possa ser feito com async function...
 
-        setTimeout(iniciaLoopDaAplicacao, TEMPO_LOOP_CONSULTA_API); // Tempo do loop.
+        // setTimeout(iniciaLoopDaAplicacao, TEMPO_LOOP_CONSULTA_API); // Tempo do loop.
     };
 
     const iniciaLoopAtualizacaoDoToken = () => {
         console.log("getTokenTimeout: ", new Date());
-        apiCalendar.tokenClient.requestAccessToken({ prompt: "none" });
-        
-        setTimeout(iniciaLoopAtualizacaoDoToken, TEMPO_LOOP_ATUALIZA_TOKEN); // token tem duração valida de 60 minutos...
-    };
+        apiCalendar.tokenClient.requestAccessToken({ prompt: "" });
 
-    function t2() {
-        apiCalendar.tokenClient.requestAccessToken({ prompt: 'consent' });
-    }
+        // setTimeout(iniciaLoopAtualizacaoDoToken, TEMPO_LOOP_ATUALIZA_TOKEN); // token tem duração valida de 60 minutos...
+    };
 
     return (
         <section className={style.app}>
@@ -150,15 +135,19 @@ export default function App() {
             </Slider>
 
             <div className={style.app__containerButton}>
-
                 <button onClick={handleItemClick}>Login</button>
-                <button onClick={t2}>T2</button>
-                <button> <a href="https://github.com/GABRIELBOLDIVEIGA" target="_blank"> GitHub </a></button>
+
+                <button>
+                    {" "}
+                    <a href="https://github.com/GABRIELBOLDIVEIGA" target="_blank">
+                        {" "}
+                        GitHub{" "}
+                    </a>
+                </button>
                 <button
                     onClick={() => {
                         iniciaLoopAtualizacaoDoToken();
                         iniciaLoopDaAplicacao();
-                        t2();
                     }}
                 >
                     Click !
